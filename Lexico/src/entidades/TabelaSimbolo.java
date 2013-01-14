86beddf7cd95a90ff045b02ef00fba612ac63d34
Gabriel Rolim <gabrielsrolim@gmail.com>
2013-01-14 10:39:32 -0300

    Primeiro commit.

---
+++ Lexico/src/entidades/TabelaSimbolo.java
@@ -0,0 +1,23 @@
+package entidades;
+
+public class TabelaSimbolo {
+	private Token token;
+	private int linha;
+	
+	
+	public Token getToken() {
+		return token;
+	}
+	public void setToken(Token token) {
+		this.token = token;
+	}
+	public int getLinha() {
+		return linha;
+	}
+	public void setLinha(int linha) {
+		this.linha = linha;
+	}
+	
+	
+
+}
---
+++ Lexico/src/entidades/Token.java
@@ -0,0 +1,26 @@
+package entidades;
+
+public class Token {
+	private int tipoToken;
+	private String Token;
+	public int getTipoToken() {
+		return tipoToken;
+	}
+	public void setTipoToken(int tipoToken) {
+		this.tipoToken = tipoToken;
+	}
+	public String getToken() {
+		return Token;
+	}
+	public void setToken(String token) {
+		Token = token;
+	}
+	
+	
+
+	
+	
+	
+	
+
+}
